2d8d7a78d5e4466a20137fad40e97e2182c61c65
semenov_da <rip13579@mail.ru>
2019-07-20 19:04:04 +0500

    layout

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'jk-cLvnqy7gdyfyv-DNoyY5FxpcYGKY7',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
